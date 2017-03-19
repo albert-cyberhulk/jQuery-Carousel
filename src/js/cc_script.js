8baa71801008b92cd737ba72604c08c98409d3eb
Mariam Martirosyan <Mariam Martirosyan>
2017-03-20 01:47:20 +0400

    removed the global reference and moved declaration inside ccCarousel

--- src/js/cc_script.js
+++ src/js/cc_script.js
@@ -7,46 +7,6 @@
 
 (function (window, $) {
 
-    var bindEvent = (function(){
-        
-            onMouseDownLeft = function(){
-                settings.slider.stop();
-                var left = settings.slider.css('left');
-                var leftMargin = parseInt(left);
-    
-                if(leftMargin <= '-'+settings.clickStep){
-                    settings.slider.animate({'left': leftMargin + settings.clickStep},settings.clickTimer);
-                }
-                else{
-                    settings.slider.animate({'left': 0},settings.clickTimer);
-                }
-            };
-            onMouseDownRight = function(){
-                settings.slider.stop();
-                var right = settings.slider.css('left');
-                var rightMargin = parseInt(right);
-    
-                var customStep = settings.addition+rightMargin;
-                //alert(customStep);                
-                if(customStep >= settings.clickStep){
-                    settings.slider.animate({'left': rightMargin - settings.clickStep},settings.clickTimer);
-                }
-                else{
-                    settings.slider.animate({'left': '-' + settings.addition},settings.clickTimer);
-                }
-            };
-            onMouseStop = function(){
-                settings.slider.stop();
-            }
-            return{
-                moveToLeft: onMouseDownLeft,
-                moveToRight: onMouseDownRight,
-                stop: onMouseStop
-            }
-        })();
-
-    var settings = {};
-
     $.fn.ccCarousel = function (options) {
 
         options = $.isPlainObject(options) ? options : {};
@@ -54,55 +14,62 @@
         /*================================Configurations==============================*/        
 
         var defaults = {
-            slider: $('.slider'),        //dynamic div
-            sliderArray: $('.slider div'),        //array of images in the slider
-            wrapperWidth: $('.galleryWrapper').width(),        //initializing the wrapper gallery width
-            dynamicWidth: $('.dynamic').width(),        //initializing the dynamic wrapper width
-            sliderLength: $('.slider div').length        // count of images in the slider
+            slider: $('.slider'), //dynamic div
+            sliderArray: $('.slider div'), //array of images in the slider
+            wrapperWidth: $('.galleryWrapper').width(), //initializing the wrapper gallery width
+            dynamicWidth: $('.dynamic').width(), //initializing the dynamic wrapper width
+            sliderLength: $('.slider div').length // count of images in the slider
         }
 
-        settings = $.extend(defaults,  $.fn.ccCarousel.defaults, options);
+        var settings = $.extend(defaults,  $.fn.ccCarousel.defaults, options);
 
-        defaults.customWidth = $('.slider div').length * (defaults.imageWidth < 50 ? 50 : defaults.imageWidth);    //the length of the slider div depending on the count of images
-        defaults.sliderWidth = defaults.customWidth;        //slider width in pixels     
-        defaults.addition = defaults.customWidth - defaults.wrapperWidth;        //the differennce between monitor reslutions       
-        defaults.clickStep = defaults.customWidth / parseInt(defaults.sliderLength);
-        defaults.slider.width(defaults.sliderWidth);        //initializing the slider width
-
-       
-        $(this).find('.leftDirection').bind('mouseover', leftMouseover);
-        $(this).find('.rightDirection').bind('mouseover', rightMouseover);
-        $(this).find('.leftDirection').bind('mouseup', leftMouseup);
-        $(this).find('.rightDirection').bind('mouseup', rightMouseup);
-        $(this).find('.leftDirection').bind('mouseout', mouseout);
-        $(this).find('.rightDirection').bind('mouseout', mouseout);
-        $(this).find('.slider img').on('click', imageClick);
-        
+        settings.customWidth = $('.slider div').length * (settings.imageWidth < 50 ? 50 : settings.imageWidth); //the length of the slider div depending on the count of images
+        settings.sliderWidth = settings.customWidth; //slider width in pixels     
+        settings.addition = settings.customWidth - settings.wrapperWidth; //the differennce between monitor reslutions       
+        settings.clickStep = settings.customWidth / parseInt(settings.sliderLength);
+        settings.slider.width(settings.sliderWidth); //initializing the slider width
 
         /*==================================main functions for click====================*/
 
-        $(this).find('.leftDirection').on('mousedown', moveToLeft);
-        $(this).find('.rightDirection').on('mousedown', moveToRight);
-
         function moveToLeft(){
-            onMouseDownLeft(); // Here I wrapped the functions, because 'onMouseDownLeft' is the same to write the code of 'moveToLeft'
+            settings.slider.stop();
+            var left = settings.slider.css('left');
+            var leftMargin = parseInt(left);
+
+            if(leftMargin <= '-' + settings.clickStep){
+                settings.slider.animate({'left': leftMargin + settings.clickStep}, settings.clickTimer);
+            }
+            else{
+                settings.slider.animate({'left': 0}, settings.clickTimer);
+            }
         }
 
         function moveToRight(){
-            onMouseDownRight(); // The same here
+            settings.slider.stop();
+            var right = settings.slider.css('left');
+            var rightMargin = parseInt(right);
+
+            var customStep = settings.addition + rightMargin;
+            //alert(customStep);                
+            if(customStep >= settings.clickStep){
+                settings.slider.animate({'left': rightMargin - settings.clickStep}, settings.clickTimer);
+            }
+            else{
+                settings.slider.animate({'left': '-' + settings.addition}, settings.clickTimer);
+            }
         }
 
         function stop(){
-            onMouseStop(); // and here
+            settings.slider.stop();
         }
 
         function leftMouseover(){
-            onMouseStop();
+            settings.slider.stop();
             var leftHover = settings.slider.css('left');
             var leftMarginHover = parseInt(leftHover);
 
             if(leftMarginHover !=0 ){
-                 settings.slider.animate({'left': 0},settings.hoverTimer);
+                 settings.slider.animate({'left': 0}, settings.hoverTimer);
             }
         }
 
@@ -111,29 +78,29 @@
             var leftMarginHover = parseInt(leftHover);
 
             if(leftMarginHover !=0 ){
-                 settings.slider.animate({'left': 0},settings.hoverTimer);
+                 settings.slider.animate({'left': 0}, settings.hoverTimer);
             }
         }        
 
         /*================================Functions that control slider when hovered==============================*/
 
         function rightMouseover(){
-            onMouseStop();
+            settings.slider.stop();
             var rightHover = settings.slider.css('left');
             var rightMarginHover = parseInt(rightHover);
 
-            settings.slider.animate({'left': '-' + settings.addition},settings.hoverTimer);
+            settings.slider.animate({'left': '-' + settings.addition}, settings.hoverTimer);
         }
 
         function rightMouseup(){
             var rightHover = settings.slider.css('left');
             var rightMarginHover = parseInt(rightHover);
 
-            settings.slider.animate({'left': '-' + settings.addition},settings.hoverTimer);
+            settings.slider.animate({'left': '-' + settings.addition}, settings.hoverTimer);
         }
 
         function mouseout(){
-            onMouseStop();
+            settings.slider.stop();
         }
 
         /*=========================================part for Images that contain links==================================================*/
@@ -146,17 +113,32 @@
                 window.location = $(this).attr('alt');
             }
         }
+
+        function bindEvents(elem){
+            elem.find('.leftDirection').on('mousedown', moveToLeft);
+            elem.find('.rightDirection').on('mousedown', moveToRight);
+            elem.find('.leftDirection').bind('mouseover', leftMouseover);
+            elem.find('.rightDirection').bind('mouseover', rightMouseover);
+            elem.find('.leftDirection').bind('mouseup', leftMouseup);
+            elem.find('.rightDirection').bind('mouseup', rightMouseup);
+            elem.find('.leftDirection').bind('mouseout', mouseout);
+            elem.find('.rightDirection').bind('mouseout', mouseout);
+            elem.find('.slider img').on('click', imageClick);        
+        }
+
+        bindEvents($(this));
+
+        $.fn.ccCarousel.moveToLeft = moveToLeft;
+        $.fn.ccCarousel.moveToRight = moveToRight;
+        $.fn.ccCarousel.stop = stop;
+
         return $(this);
     };
 
     $.fn.ccCarousel.defaults = {
-        clickTimer:250,        //setTimeout for click
-        hoverTimer: 7000,        //setTimeout for hover
-        imageWidth: 330
+            clickTimer:250, //setTimeout for click
+            hoverTimer: 7000, //setTimeout for hover
+            imageWidth: 330
     }
-
-    $.fn.ccCarousel.moveToLeft = onMouseDownLeft;
-    $.fn.ccCarousel.moveToRight = onMouseDownRight;
-    $.fn.ccCarousel.stop = onMouseStop;
 
 }(window, jQuery));//end of dom load
